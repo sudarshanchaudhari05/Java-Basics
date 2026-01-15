a3654809bb5918d079748de55358f8c2b9c7973a
sudarshan <sudarshanchaudhari05@gmail.com>
2026-01-15 20:44:55 +0530

    new file secend

---
+++ second.java
@@ -0,0 +1,20 @@
+class second {
+    static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int number = 29; // Example number
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
