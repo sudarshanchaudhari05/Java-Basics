a91899c6faa8ae175feb4f42c4c9c93ab2892efc
sudarshan <sudarshanchaudhari05@gmail.com>
2026-01-15 14:18:32 +0530

    First code

---
+++ Trial 1.java
@@ -0,0 +1,9 @@
+class First {
+  public static void main (String[] args)  {
+    System.out.println("Hello World");
+    int Num1=7;
+    int Num2=8;
+    int sum= Num1+ Num2;
+    System.out.println(sum);
+  }
+}
---
+++ litrals {.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class scanner {
+    public static void main(String[] args) {
+        int a,b;
+        char ch='A';
+        float fl=5.8f;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No. 1");
+        a = sc.nextInt();
+        System.out.println("enter No. 2");
+        b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
